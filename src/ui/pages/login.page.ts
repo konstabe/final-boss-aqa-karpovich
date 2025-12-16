e8cb327319d6a7a600e7244c880dd699ee2b23d9
konstabe <konstantingames94@gmail.com>
2025-12-16 21:49:34 +0200

    base pages

---
+++ src/ui/pages/login.page.ts
@@ -0,0 +1,21 @@
+import { ICredentials } from "data/types/credentials.types.js";
+import { logStep } from "utils/report/logStep.utils.js";
+import { SalesPortalPage } from "./salesPortal.page.js";
+
+export class LoginPage extends SalesPortalPage {
+	readonly emailInput = this.page.locator("#emailinput");
+	readonly passwordInput = this.page.locator("#passwordinput");
+	readonly loginButton = this.page.locator("button[type='submit']");
+	readonly uniqueElement = this.page.locator("#signInPage");
+
+	@logStep("Fill credentials")
+	async fillCredentials(credentials: Partial<ICredentials>) {
+		if (credentials.username) await this.emailInput.fill(credentials.username);
+		if (credentials.password) await this.passwordInput.fill(credentials.password);
+	}
+
+	@logStep("Click login button")
+	async clickLogin() {
+		await this.loginButton.click();
+	}
+}
