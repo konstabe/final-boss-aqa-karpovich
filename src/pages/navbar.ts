b0b6d68c327f52ec7fb33f0b779c2fdfe7a3c636
LubovLepekhina <lubov.lepekhina@gmail.com>
2025-12-20 21:32:11 +0100

    fixed

--- src/pages/navbar.ts
+++ src/pages/navbar.ts
@@ -4,8 +4,9 @@ import { BasePage } from "./base/base.page";
 export type Modules = "Home" | "Products" | "Customers" | "Orders" | "Managers";
 
 export class NavBar extends BasePage {
-	readonly modules = this.page.locator(".d-none div[name='module-item'] a");
-	readonly moduleByName = (modulesName: Modules) => this.modules.filter({ hasText: modulesName });
+	readonly navigationBar = this.page.locator("#main-header");
+	readonly moduleByName = (moduleName: Modules) =>
+		this.navigationBar.locator(`a[name="${moduleName.toLowerCase()}"]`);
 
 	@logStep("Click Module on Navigation Menu")
 	async clickModule(modulesName: Modules) {
