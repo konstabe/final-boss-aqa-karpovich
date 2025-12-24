7580137e129a176478c8f046b434e0a3d6128717
LubovLepekhina <113927670+LubovLepekhina@users.noreply.github.com>
2025-12-24 12:09:07 +0100

    Tests for Reopen modal on ordersList Page (#60)
    * added test for Reopen modal on ordersList Page
    
    * fixed
    
    * fixed

---
+++ src/data/orders/confirmationModalText.ts
@@ -0,0 +1,8 @@
+export const CONFIRMATION_MODAL_TEXT = {
+	reopen: {
+		title: "Reopen Order",
+		body: "Are you sure you want to reopen the order?",
+		confirmButton: "Yes, Reopen",
+	},
+	cancelButton: "Cancel",
+};
