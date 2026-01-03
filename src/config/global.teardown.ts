1347b5963737919346981d3e34920cc0aa4b85bb
maratgefter <47002971+maratgefter@users.noreply.github.com>
2026-01-03 18:02:37 +0300

    History try (#57)
    add history to allure

--- src/config/global.teardown.ts
+++ src/config/global.teardown.ts
@@ -6,9 +6,12 @@ export default async function () {
 
 	const notificationService = new NotificationService(new TelegramService());
 
+	const reportPath = process.env.REPORT_PATH || "allure-report";
+	const reportUrl = `https://konstabe.github.io/final-boss-aqa-karpovich/${reportPath}`;
+
 	await notificationService.postNotification(`Test run finished!
     
 Link to deployed report:
 
-https://konstabe.github.io/final-boss-aqa-karpovich/allure-report/#`);
+${reportUrl}`);
 }
